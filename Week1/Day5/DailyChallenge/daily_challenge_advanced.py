29f2425c8a852af2086c7a0c15c4a2e3ddee155c
Vlad Grinberg <vlaaad.97@gmail.com>
2024-08-11 17:15:16 +0300

    previous week exercises

--- Week1/Day5/DailyChallenge/daily_challenge_advanced.py
+++ Week1/Day5/DailyChallenge/daily_challenge_advanced.py
@@ -17,28 +17,3 @@ for num in list_of_numbers:
 
 
 print(pair_count)
-
-
-# Dictionary to store the frequency of each number
-num_dict = {}
-
-# Variable to count the number of pairs
-pair_count = 0
-
-# Iterate through each number in the list
-for number in list_of_numbers:
-    # Calculate the complement number that, when added to the current number, equals the target_number
-    complement = target_number - number
-    
-    # If the complement exists in the dictionary, it means we've found a pair
-    if complement in num_dict:
-        pair_count += num_dict[complement]
-    
-    # Add the current number to the dictionary or update its frequency
-    if number in num_dict:
-        num_dict[number] += 1
-    else:
-        num_dict[number] = 1
-
-# Print the total number of pairs found
-print(f"The number of pairs that sum to {target_number} is: {pair_count}")
