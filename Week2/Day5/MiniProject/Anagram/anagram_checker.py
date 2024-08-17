252f775fa4a1baa94b6e5ed62ea41148c47f27f7
Vlad Grinberg <vlaaad.97@gmail.com>
2024-08-17 22:29:18 +0300

    dailu challenge quizz solution

--- Week2/Day5/MiniProject/Anagram/anagram_checker.py
+++ Week2/Day5/MiniProject/Anagram/anagram_checker.py
@@ -1,14 +1,3 @@
-# The class should have the following methods:
-# __init__ - should load the word list file (text file) into a variable, so that it can be searched later on in the code.
-# is_valid_word(word) – should check if the given word (ie. the word of the user) is a valid word.
-
-# get_anagrams(word) – should find all anagrams for the given word. (eg. if word of the user is ‘meat’, the function should return a list containing [“mate”, “tame”, “team”].)
-
-# Hint: you might want to create a separate method called is_anagram(word1, word2), that will compare 2 words and return True if they contain the same letters (but not in the same order), and False if not.
-
-# Note: None of the methods in the class should print anything.
-
-
 from collections import Counter
 
 
@@ -23,7 +12,7 @@ class AnagramChecker:
         elif file_path is not None:
             return self.load_words_from_file(file_path)
         else:
-            raise ValueError("Either 'words' or 'file_path' must be provided")
+            raise Exception("Either 'words' or 'file_path' must be provided")
 
     @staticmethod
     def load_words_from_file(file_path):
