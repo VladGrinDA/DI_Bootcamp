0ad0efff6d07b19fe9bf93c28ae1eabd0f67f4c2
Vlad Grinberg <vlaaad.97@gmail.com>
2024-08-19 15:58:34 +0300

    small fixes

--- Week3/Day2/DailyChallenge/dc_puzzle.sql
+++ Week3/Day2/DailyChallenge/dc_puzzle.sql
@@ -1,5 +1,7 @@
 -- Active: 1723968844525@@127.0.0.1@5432@public@public
 
+drop table if exists firsttab;
+
 CREATE TABLE FirstTab (
      id integer, 
      name VARCHAR(10)
@@ -24,23 +26,26 @@ INSERT INTO SecondTab
 
 SELECT * FROM SecondTab;
 
--- Prediction: Count rows from FirstTab where id is not NULL. So it shoud return 6
+-- Prediction: Count rows from FirstTab where id is not NULL. So it shoud return 3
 SELECT COUNT(*) 
 FROM FirstTab AS ft WHERE ft.id NOT IN (SELECT id FROM SecondTab WHERE id IS NULL) -- 0
 
--- Prediction: Last one was a suprise. If condition including null returns null then the output shold be 4
+-- Prediction: Last one was a suprise. If condition including null returns null then the output shold be 2
 SELECT COUNT(*) 
-FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 ) -- 4
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 ) -- 2
 
 -- Prediction: i hope it's 4
 SELECT COUNT(*) 
 FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab ) -- 0
 
 -- OKAY. NOW I SEE
--- select 6 not in (5, NULL) -- NULL
--- select 6 in (5, NULL) -- NULL
--- select 5 in (5, NULL) -- true
+select 6 in (NULL); -- NULL;
+select 6 in (5, NULL); -- NULL
+select 5 in (5, NULL); -- true
+select 5 not in (5, NULL); -- false
 
--- Prediction: At that point I feel like you must have abilities to see the future. I've acquired some so the answer is 4
+select 5 + NULL; -- NULL
+
+-- Prediction: At that point I feel like you must have abilities to see the future. I've acquired some so the answer is 2
 SELECT COUNT(*) 
-FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL ) -- 4
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL ) -- 2
